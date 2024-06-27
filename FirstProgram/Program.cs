4d018248dd05921222b5aa350a5d9b19a4874858
saikumar.bhamini <saikumar.bhamini@innovasolutions.com>
2024-06-27 12:40:56 +0530

    operator overloading for matrix sum and difference

--- FirstProgram/Program.cs
+++ FirstProgram/Program.cs
@@ -1,6 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome!");
-for(int i = 0; i < 10; i++){
-    Console.WriteLine("Hello World");
-}
+﻿class Matrix
+{
+    readonly public int a, b, c, d;
+    public Matrix(int a, int b, int c, int d)
+    {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+    }
 
+    public static Matrix operator +(Matrix A, Matrix B)
+    {
+        Matrix C = new(A.a + B.a, A.b + B.b, A.c + B.c, A.d + B.d);
+        return C;
+    }
+    public static Matrix operator -(Matrix A, Matrix B)
+    {
+        Matrix C = new(A.a - B.a, A.b - B.b, A.c - B.c, A.d - B.d);
+        return C;
+    }
+    
+    public override string ToString()
+    {
+        return $"{this.a} {this.b}\n{this.c} {this.d}\n";
+    }
+    
+}
+class TestMatrix
+{
+    static void Main()
+    {
+        Matrix m1 = new(4, 5, 7, 3);
+        Matrix m2 = new(-6, 2, 11, 13);
+        Matrix sum = m1 + m2;
+        Matrix difference = m1 - m2;
+        Console.WriteLine(sum);
+        Console.WriteLine(difference);
+    }
+}
