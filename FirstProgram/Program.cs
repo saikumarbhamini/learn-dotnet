efd8163f7d2345b1ec6a5de348a948f2c266fdba
saikumar.bhamini <saikumar.bhamini@innovasolutions.com>
2024-06-27 17:03:12 +0530

    implemented interface and its method in a child class

--- FirstProgram/Program.cs
+++ FirstProgram/Program.cs
@@ -1,6 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome!");
-for(int i = 0; i < 10; i++){
-    Console.WriteLine("Hello World");
+﻿interface IAddition
+{
+    void Add(int a, int b);
 }
+interface ISubtraction: IAddition
+{
+    void Sub(int a, int b);
+}
+class TestInterface: ISubtraction
+{
+    public void Add(int a, int b)
+    {
+        Console.WriteLine(a + b);
+    }
 
+    public void Sub(int a, int b)
+    {
+        Console.WriteLine(a - b);
+    }
+    
+    static void Main()
+    {
+        TestInterface obj = new();
+        obj.Add(411, -6);
+        obj.Sub(21, 3);
+    }
+}
