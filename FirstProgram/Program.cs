7aa2d244648db7d8776e72eca78882f5d51e252d
saikumarbhamini <saibamini@gmail.com>
2024-07-01 11:23:53 +0530

    properties-and-auto-implemented-property

--- FirstProgram/Program.cs
+++ FirstProgram/Program.cs
@@ -1,16 +1,26 @@
-﻿class Customer
+﻿using System.Linq;
+
+public enum Cities
+{
+    Hyderabad = 1, Mumbai, Bangalore, Chennai, Delhi
+}
+
+class Customer
 {
     readonly int _CustId;
     string _Cname;
     bool _Status;
     double _Balance;
+    Cities _City;
 
-    public Customer(int CustId, bool Status, string Cname, double Balance)
+    public Customer(int CustId, bool Status, string Cname, double Balance, Cities City, string Country)
     {
         _CustId = CustId;
         _Cname = Cname;
         _Status = Status;
         _Balance = Balance;
+        _City = City;
+        this.Country=Country;
     }
 
     public int CustId
@@ -20,7 +30,11 @@
     public string Cname
     {
         get { return _Cname; }
-        set { _Cname = value; }
+        set
+        {
+            if (_Status == true)
+                _Cname = value;
+        }
     }
     public bool Status
     {
@@ -30,16 +44,18 @@
     public double Balance
     {
         get { return _Balance; }
-        set {
+        set
+        {
             Console.WriteLine("...Requested to change balance");
-            if(_Status==true && value >= 500)
+            if (_Status == true && value >= 500)
             {
                 _Balance = value;
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("..Passed");
                 Console.ForegroundColor = ConsoleColor.Black;
             }
-            else {
+            else
+            {
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("..Failed");
                 Console.ForegroundColor = ConsoleColor.Black;
@@ -48,23 +64,43 @@
 
         }
     }
+
+    public Cities City
+    {
+        get { return _City; }
+        set
+        {
+            if (_Status == true)
+                _City = value;
+        }
+    }
+
+    public string Country  // Auto implemented or automatic property
+    {
+        get;
+        set;
+    }
 }
 
 class Program
 {
     static void Main()
     {
-        Customer Cust = new(100, true, "Saikumar", 43000);
+        Customer Cust = new(100, true, "Saikumar", 43000, Cities.Hyderabad, "India");
         Console.WriteLine($"{Cust.Cname}");
+        Console.WriteLine($"City: {Cust.City}");
         Console.WriteLine($"Initial Balance: {Cust.Balance}");
         Cust.Balance -= 2021;
+        Cust.City = Cities.Bangalore;
         Console.WriteLine(Cust.Balance);
-        Cust.Status=false;
+        Cust.Status = false;
         Cust.Balance -= 1100;
         Console.WriteLine(Cust.Balance);
-        Cust.Status=true;
+        Cust.Status = true;
         Cust.Balance -= 44000;
         Console.WriteLine(Cust.Balance);
+        Console.WriteLine(Cust.City);
+        Console.WriteLine(Cust.Country);
     }
-    
+
 }
