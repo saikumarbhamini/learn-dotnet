0b450eda8bca672bba8b646f6faaf8dd30c54704
saikumarbhamini <saibamini@gmail.com>
2024-07-05 11:57:54 +0530

    exception handling and created custom Application exception

--- FirstProgram/Program.cs
+++ FirstProgram/Program.cs
@@ -1,6 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome!");
-for(int i = 0; i < 10; i++){
-    Console.WriteLine("Hello World");
-}
+﻿namespace FirstProgram
+{
 
+    public class DividedByOddNumberException : ApplicationException
+    {
+        public override string Message
+        {
+            get
+            {
+                return "Attempted to divide by odd number";
+            }
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            int x, y;
+            try
+            {
+                Console.Write("Enter first number:");
+                x = int.Parse(Console.ReadLine());
+                Console.Write("Enter second number:");
+                y = int.Parse(Console.ReadLine());
+                if (y % 2 != 0)
+                    // throw new ApplicationException("Attempted to divide by odd number.");
+                    throw new DividedByOddNumberException();
+                int z = x / y;
+                Console.WriteLine("Result:" + z);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (DividedByOddNumberException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("End of the program");
+            }
+
+        }
+    }
+}
